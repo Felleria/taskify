c51673325ced15924e6c9c9fe1660947c6ddea3c
Felleria <tellytabbie@gmail.com>
2025-01-18 18:41:14 +0300

    🔧 Install react-tsparticles tsparticles
    install react-tsparticles tsparticles

---
+++ src/components/Todo.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import todo_icon from '../assets/todo_icon.png'
+
+
+const Todo = () => {
+  return (
+    <div className='bg-slate-500 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+        {/*-----title-----*/}
+
+        <div className='flex items-center mt-7 gap-2'>
+            <img className='w-8' src={todo_icon} alt="" />
+            <h1 className='text-3xl font-semibold'>Get Shit Done!</h1>
+
+
+        </div>
+
+        {/*-----input box-----*/}
+        <div className='flex items-center my-7 bg-gray-200 rounded-full' >
+            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add tasks today' />
+            <button className='border-none rounded-full  bg-purple-500 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        </div>
+
+        {/*-----Todo list-----*/}
+
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Todo
